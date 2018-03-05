38761483b27e716166ba5c249e7104b3144c473f
mr5 <u@mr-5.cn>
2018-03-05 19:00:58 +0800

    initial commits

---
+++ collector.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import threading
+import os
+import errno
+from urllib3.exceptions import HTTPError
+
+
+class Collector(threading.Thread):
+    def __init__(self, client, container, log_type, log_path='/var/log/swarm-logger/'):
+        super().__init__()
+        self.stop_event = threading.Event()
+        self.reopen_event = threading.Event()
+        self.client = client
+        self.log_path = log_path
+        self.log_type = log_type
+        self.container = container
+        self.file = self.open_container_log_file(container=self.container, log_type=self.log_type)
+
+    def join(self, timeout=None):
+        self.stop()
+        super(Collector, self).join(timeout)
+
+    def stop(self):
+        self.stop_event.set()
+
+    def reopen_log_file(self):
+        print('Reopening log file.')
+        self.file.close()
+        self.file = self.open_container_log_file(container=self.container, log_type=self.log_type)
+
+    def run(self):
+        while not self.stop_event.isSet():
+            # 通知重新打开日志文件
+            if self.reopen_event.isSet():
+                self.reopen_log_file()
+                self.reopen_event.clear()
+            try:
+                self.collect_container_logs(container=self.container, log_type=self.log_type)
+            except HTTPError as he:
+                print(he)
+            except Exception as e:
+                print(e)
+
+    def open_container_log_file(self, container, log_type):
+        labels = container.labels
+        log_path = self.log_path + labels['com.docker.compose.project']
+        log_file = log_path + '/' + labels['com.docker.compose.service'] + '_' + labels[
+            'com.docker.compose.container-number'] + '.' + log_type
+        if not os.path.exists(log_path):
+            try:
+                os.makedirs(log_path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+        return open(log_file, 'a')
+
+    def collect_container_logs(self, container, log_type):
+        while True:
+            for line in container.logs(stream=True, stderr=log_type == 'both' or log_type == 'err',
+                                       stdout=log_type == 'both' or log_type == 'out'):
+                self.file.write(line.decode("utf-8"))
---
+++ swarm_logger.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import docker
+import time
+import signal
+import os
+from collector import Collector
+
+
+class Manager:
+    def __init__(self, client):
+        self.client = client
+        self.log_label_prefix = 'mr5.swarmlogger.type'
+        self.threads = {}
+
+    def main(self):
+        pidfile = open('./swarm-logger.pid', 'w')
+        pidfile.write(str(os.getpid()))
+        pidfile.close()
+        while True:
+            try:
+                alive_threads = 0
+                for container in self.client.containers.list({'status': 'running'}):
+                    for label_key in container.labels:
+                        if label_key.startswith(self.log_label_prefix):
+                            alive_threads += 1
+                            if container.id in self.threads and self.threads[container.id].is_alive():
+                                continue
+                            collector_thread = Collector(client=self.client, container=container,
+                                                         log_type=container.labels[label_key])
+                            collector_thread.setDaemon(True)
+                            collector_thread.start()
+                            self.threads[container.id] = collector_thread
+                print('threads: ' + str(alive_threads) + '/' + str(len(self.threads)))
+            except Exception as e:
+                print(e)
+            time.sleep(1)
+
+    def reopen_log_files(self, signal, frame):
+        for container_id in self.threads:
+            thread = self.threads[container_id]
+            if thread.is_alive():
+                thread.reopen_log_file()
+
+
+if __name__ == '__main__':
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    manager = Manager(client=client)
+    signal.signal(signal.SIGUSR1, manager.reopen_log_files)
+    manager.main()
