7b36a43f0c04f773b07a806a5283d698077ee276
mr5 <u@mr-5.cn>
2018-03-06 15:50:57 +0800

    support multiple labels

--- swarm_logger.py
+++ swarm_logger.py
@@ -21,15 +21,25 @@ class Manager:
                 alive_threads = 0
                 for container in self.client.containers.list({'status': 'running'}):
                     for label_key in container.labels:
-                        if label_key.startswith(self.log_label_prefix):
-                            alive_threads += 1
-                            if container.id in self.threads and self.threads[container.id].is_alive():
+                        if not label_key.startswith(self.log_label_prefix):
+                            continue
+                        alive_threads += 1
+                        if container.id in self.threads:
+                            if label_key in self.threads[container.id] \
+                                    and self.threads[container.id][label_key].is_alive():
                                 continue
-                            collector_thread = Collector(client=self.client, container=container,
-                                                         log_type=container.labels[label_key])
-                            collector_thread.setDaemon(True)
-                            collector_thread.start()
-                            self.threads[container.id] = collector_thread
+                        else:
+                            self.threads[container.id] = {}
+                        collector_thread = Collector(client=self.client, container=container,
+                                                     log_type=container.labels[label_key])
+                        collector_thread.setDaemon(True)
+                        collector_thread.start()
+                        self.threads[container.id][label_key] = collector_thread
+                    if container.id not in self.threads:
+                        continue
+                    for label_key in self.threads[container.id]:
+                        if label_key not in container.labels:
+                            self.threads[container.id][label_key].stop()
                 print('threads: ' + str(alive_threads) + '/' + str(len(self.threads)))
             except Exception as e:
                 print(e)
@@ -37,9 +47,10 @@ class Manager:
 
     def reopen_log_files(self, signal, frame):
         for container_id in self.threads:
-            thread = self.threads[container_id]
-            if thread.is_alive():
-                thread.reopen_log_file()
+            for label_key in self.threads[container_id]:
+                thread = self.threads[container_id][label_key]
+                if thread.is_alive():
+                    thread.reopen_log_file()
 
 
 if __name__ == '__main__':
